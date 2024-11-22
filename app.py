ef9748e4c8e656a2bf7c63e60dd88dbdf27fb01a
Rehima Kemal <67173657+Rehima1@users.noreply.github.com>
2024-11-22 23:18:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,32 +8,47 @@ app = Flask(__name__)
 CORS(app)
 
 
-with open("model.pkl", "rb") as f:
+model_path = "C:\Users\Rehima\OneDrive\سطح المكتب\JS\model.pkl" 
+with open(model_path, "rb") as f:
     model = pickle.load(f)
 
 @app.route("/", methods=["GET"])
 def home():
     return "Welcome to the Customer Churn Prediction API!"
 
+def preprocess_geography(geography):
+    if geography == "Germany":
+        return [1, 0]
+    elif geography == "Spain":
+        return [0, 1]
+    elif geography == "France":  
+        return [0, 0]
+    else:
+        raise ValueError("Invalid Geography value")
+
 @app.route("/predict", methods=["POST"])
 def predict():
     try:
         data = request.json
-        
-        Features = np.array([
-            data['CreditScore'],
-            data['Geography'],  
-            0 if data['Gender'] == "Male" else 1,  
-            data['Age'],
-            data['Tenure'],
-            data['Balance'],
-            data['NumOfProducts'],
-            data['HasCrCard'],
-            data['IsActiveMember'],
-            data['EstimatedSalary']
-        ]).reshape(1, -1)
-        
+        print("Received Data:", data)  
+
+        geography_encoded = preprocess_geography(data['Geography'])
+        gender_encoded = 0 if data['Gender'] == "Male" else 1 
+
        
+        Features = np.array([
+            data['CreditScore'],     
+            gender_encoded,          
+            data['Age'],            
+            data['Tenure'],          
+            data['Balance'],         
+            data['NumOfProducts'],   
+            data['HasCrCard'],       
+            data['IsActiveMember'],  
+            data['EstimatedSalary'], 
+            *geography_encoded       
+        ]).reshape(1, -1)
+
         prediction = model.predict(Features)[0]
 
         return jsonify({"prediction": int(prediction)})
@@ -41,7 +56,6 @@ def predict():
     except Exception as e:
         return jsonify({"error": str(e)}), 400
 
-
 if __name__ == "__main__":
     port = os.getenv("PORT", 5000)  
-    app.run(host="0.0.0.0", port=int(port), debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
